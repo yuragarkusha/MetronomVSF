ba3b5cf2e28f8d58af399fd1586320abffd3143b
Ge0rge <yura.garik@gmail.com>
2016-03-25 06:00:20 +0200

    Initial commit

---
+++ app/src/main/java/com/yuriy/harkusha/metronome/MainActivity.java
@@ -0,0 +1,163 @@
+package com.yuriy.harkusha.metronome;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    private boolean serviceIsWork = false;
+    private EditText editBPM;
+    private SeekBar seekBar;
+    private int bpm;
+    private boolean vibrationOn = false;
+    private boolean lightOn = false;
+    private boolean soundOn = false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        editBPM = (EditText) findViewById(R.id.bpmEditText);
+        bpm = Integer.parseInt(String.valueOf(editBPM.getText()));
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                bpm = progress;
+                editBPM.setText(Integer.toString(bpm));
+            }
+        });
+
+    }
+    public void startStopButtonClick(View view) {
+        if (!serviceIsWork) {
+            onClickStart();
+            updateElementsAfterStartService();
+        } else {
+            onClickStop();
+            updateElementsAfterStopService();
+        }
+    }
+
+    public void onClickStart() {
+        serviceIsWork = true;
+        Intent intent = new Intent(this, MetronomeService.class);
+        int getText = (Integer.parseInt(String.valueOf(editBPM.getText())));
+        if (getText > 200) {
+            getText = 200;
+        }
+        bpm = getText;
+        intent.putExtra("bpm", bpm);
+        intent.putExtra("vibrationOn", vibrationOn);
+        intent.putExtra("lightOn", lightOn);
+        intent.putExtra("soundOn", soundOn);
+        startService(intent);
+
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(editBPM, InputMethodManager.SHOW_IMPLICIT);
+    }
+
+    public void onClickStop() {
+        Intent stopIntent = new Intent(MainActivity.this, MetronomeService.class);
+        stopService(stopIntent);
+        serviceIsWork = false;
+    }
+
+    public void updateElementsAfterStartService() {
+        Button button = (Button) findViewById(R.id.buttonStartStop);
+        button.setText("STOP");
+        button.setBackgroundDrawable(getResources().getDrawable(R.drawable.stop_button));
+
+        ImageView indicator = (ImageView) findViewById(R.id.indicator);
+        indicator.setImageDrawable(getResources().getDrawable(R.drawable.indicator_on));
+    }
+
+    public void updateElementsAfterStopService() {
+        Button button = (Button) findViewById(R.id.buttonStartStop);
+        button.setText("START");
+        button.setBackgroundDrawable(getResources().getDrawable(R.drawable.start_button));
+
+        ImageView indicator = (ImageView) findViewById(R.id.indicator);
+        indicator.setImageDrawable(getResources().getDrawable(R.drawable.indicator_off));
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+    }
+
+    public void onVibrationToggleClick(View view) {
+        TextView vibrationTextView = (TextView) findViewById(R.id.textView_vibration);
+        ImageView vibrationImageView = (ImageView) findViewById(R.id.imageView_vibration);
+
+        if (((ToggleButton) view).isChecked()) {
+            vibrationImageView.setImageResource(R.drawable.ic_vibration_on);
+            vibrationTextView.setTextColor(getResources().getColor(R.color.colorForOn));
+            vibrationOn = true;
+        } else {
+            vibrationImageView.setImageResource(R.drawable.ic_vibration_off);
+            vibrationTextView.setTextColor(getResources().getColor(R.color.colorForOff));
+            vibrationOn = false;
+        }
+    }
+
+    public void onFlashlightToggleClick(View view) {
+        TextView flashlightTextView = (TextView) findViewById(R.id.textView_flashlight);
+        ImageView flashlightImageView = (ImageView) findViewById(R.id.imageView_flashlight);
+
+        if (((ToggleButton) view).isChecked()) {
+            flashlightImageView.setImageResource(R.drawable.ic_flashlight_on);
+            flashlightTextView.setTextColor(getResources().getColor(R.color.colorForOn));
+            lightOn = true;
+        } else {
+            flashlightImageView.setImageResource(R.drawable.ic_flashlight_off);
+            flashlightTextView.setTextColor(getResources().getColor(R.color.colorForOff));
+            lightOn = false;
+        }
+    }
+
+    public void onSoundToggleClick(View view) {
+        TextView soundTextView = (TextView) findViewById(R.id.textView_sound);
+        ImageView soundImageView = (ImageView) findViewById(R.id.imageView_sound);
+
+        if (((ToggleButton) view).isChecked()) {
+            soundImageView.setImageResource(R.drawable.ic_sound_on);
+            soundTextView.setTextColor(getResources().getColor(R.color.colorForOn));
+            soundOn = true;
+        } else {
+            soundImageView.setImageResource(R.drawable.ic_sound_off);
+            soundTextView.setTextColor(getResources().getColor(R.color.colorForOff));
+            soundOn = false;
+        }
+    }
+}
---
+++ app/src/main/java/com/yuriy/harkusha/metronome/MetronomeService.java
@@ -0,0 +1,137 @@
+package com.yuriy.harkusha.metronome;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.hardware.Camera;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.os.Vibrator;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MetronomeService extends Service {
+
+
+    public Context context = this;
+    private boolean status;
+    private int bpm;
+    private int milliseconds = 0;
+    private boolean vibrationOn;
+    private boolean lightOn;
+    private boolean soundOn;
+    private int vibrationDuration = 100;
+    private boolean serviceStatus;
+
+    public void onCreate() {
+        Toast.makeText(this, "Служба создана",
+                Toast.LENGTH_SHORT).show();
+        serviceStatus = true;
+    }
+
+    public void onDestroy() {
+        Toast.makeText(this, "Служба остановлена",
+                Toast.LENGTH_SHORT).show();
+        serviceStatus = false;
+    }
+
+    public void runMetronome() {
+        Timer timer = new Timer("MetronomeTimer", true);
+        TimerTask metronomeTimer = new TimerTask() {
+            @Override
+            public void run() {
+                if (serviceStatus) {
+                    if (soundOn) {
+                        sound();
+                    }
+                    if (vibrationOn) {
+                        vibrate(vibrationDuration);
+                    }
+                    if (lightOn) {
+                        light();
+                    }
+                } else ;
+            }
+        };
+        if (milliseconds != 0) {
+            timer.scheduleAtFixedRate(metronomeTimer, milliseconds, milliseconds);
+        }
+    }
+
+    public int bmpToMilliseconds(int bmp) {
+        int msInMinute = 60000;
+        if (bpm != 0) {
+            milliseconds = msInMinute / bmp;
+        } else {
+            milliseconds = 0;
+        }
+        return milliseconds;
+    }
+
+    public void vibrate(int duration) {
+        Vibrator vibration = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibration.vibrate(duration);
+    }
+
+    public void light() {
+        if (!status) {
+            Camera camera = Camera.open();
+            Camera.Parameters cameraParameters = camera.getParameters();
+            cameraParameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(cameraParameters);
+            camera.startPreview();
+            status = true;
+            camera.stopPreview();
+            camera.release();
+            status = false;
+        }
+    }
+
+    public void sound() {
+        MediaPlayer player = MediaPlayer.create(this, R.raw.tic);
+        player.setLooping(false);
+        player.start();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        try {
+            bpm = intent.getIntExtra("bpm", 0);
+            vibrationOn = intent.getBooleanExtra("vibrationOn", false);
+            lightOn = intent.getBooleanExtra("lightOn", false);
+            soundOn = intent.getBooleanExtra("soundOn", false);
+            milliseconds = bmpToMilliseconds(bpm);
+            saveSettings();
+        } catch (NullPointerException ignored) {
+            loadSettings();
+        }
+        runMetronome();
+        return START_NOT_STICKY;   ////Or START_STICKY if you want to restart service after you close Activity
+    }
+
+    public void saveSettings() {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt("bpm", bpm);
+        editor.putBoolean("vibrationOn", vibrationOn);
+        editor.putBoolean("lightOn", lightOn);
+        editor.putBoolean("soundOn", soundOn);
+        editor.commit();
+    }
+
+    public void loadSettings() {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        bpm = settings.getInt("bpm", 0);
+        vibrationOn = settings.getBoolean("vibrationOn", false);
+        lightOn = settings.getBoolean("lightOn", false);
+        soundOn = settings.getBoolean("soundOn", false);
+    }
+}
